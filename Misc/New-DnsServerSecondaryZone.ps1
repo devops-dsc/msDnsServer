21849f3cac4cd7c5e61df16be596003883a6668c
Jon Madison <Jonathan.madison@rackspace.com>
2015-01-09 15:19:13 -0600

    refactor

--- Misc/New-DnsServerSecondaryZone.ps1
+++ Misc/New-DnsServerSecondaryZone.ps1
@@ -1,4 +1,4 @@
-﻿$Properties = @{
+$Properties = @{
                 Name      = New-xDscResourceProperty -Name Name -Type String -Attribute Key `
                                                      -Description 'Name of the secondary zone'
                 DnsServer = New-xDscResourceProperty -Name MasterServerIPAddress -Type String[] -Attribute Required `
@@ -7,4 +7,4 @@
                                                      -Description 'Should this resource be present or absent'
                 
             }
-New-xDscResource -Name MSFT_xDnsServerSecondaryZone -Property $Properties.Values -Path . -ModuleName xDnsServer -FriendlyName xDnsServerSecondaryZone -Force
+New-xDscResource -Name MSFT_msDnsServerSecondaryZone -Property $Properties.Values -Path . -ModuleName xDnsServer -FriendlyName msDnsServerSecondaryZone -Force
